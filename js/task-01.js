0887243fae6561149d5dce2113ea1a3f2e20337e
Vitalii Morgunov <utlab13@gmail.com>
2021-06-06 12:21:55 +0200

    Update task 1

--- js/task-01.js
+++ js/task-01.js
@@ -1,25 +1,6 @@
 const categoriesRef = document.querySelectorAll('.item');
 console.log('В списке ' + categoriesRef.length + ' категории');
 
-// listRef[0].classList.add('item-list__animals');
-// listRef[1].classList.add('item-list__food');
-// listRef[2].classList.add('item-list__tech');
-
-// console.log('Категория: ', titelRef[0].textContent);
-// console.log('Количество: ', listRef[0].children.length);
-// console.log('Категория: ', titelRef[1].textContent);
-// console.log('Количество: ', listRef[1].children.length);
-// console.log('Категория: ', titelRef[2].textContent);
-// console.log('Количество: ', listRef[2].children.length);
-
-// const titel = titelRef.forEach(titel =>
-//   console.log('Категория: ', titel.textContent),
-// );
-
-// const list = listRef.forEach(li =>
-//   console.log('Количество элементов: ', li.children.length),
-// );
-
 const categoriesListRef = document.querySelector('#categories').children;
 
 for (let item of categoriesListRef) {
