5dba68ca6e2359185ba418d0b5f7ae4f61a71404
Vitalii Morgunov <utlab13@gmail.com>
2021-06-06 12:26:30 +0200

    Update task5

--- js/task-05.js
+++ js/task-05.js
@@ -4,9 +4,7 @@ const outputRef = document.getElementById('name-output');
 inputRef.addEventListener('input', onInputChange);
 
 function onInputChange(e) {
-  if (e.currentTarget.value === '') {
-    outputRef.textContent = 'незнакомец';
-  } else {
-    outputRef.textContent = e.currentTarget.value;
-  }
+  e.currentTarget.value === ''
+    ? (outputRef.textContent = 'незнакомец')
+    : (outputRef.textContent = e.currentTarget.value);
 }
